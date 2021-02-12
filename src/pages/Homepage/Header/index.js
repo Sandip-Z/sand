f62416540451f64f5e050b3345ceb6770464aa98
sandip satyal <sandipsatyal25@gmail.com>
2021-02-12 17:10:18 +0545

    header layout;

--- src/pages/Homepage/Header/index.js
+++ src/pages/Homepage/Header/index.js
@@ -1,5 +1,13 @@
 const HeaderSection = () => {
-  return <p>This is HeaderSection</p>;
+  return (
+    <header className="w-full h-screen bg-gray-900 text-white">
+      <div className="flex">
+        <div className="border w-1/6">one</div>
+        <div className="border w-5/12">two</div>
+        <div className="border w-5/12">three</div>
+      </div>
+    </header>
+  );
 };
 
 export default HeaderSection;
