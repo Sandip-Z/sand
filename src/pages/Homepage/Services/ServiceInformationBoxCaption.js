cafcf4ca7d0630416aa9d0152135967ef22f93e6
sandip satyal <sandipsatyal25@gmail.com>
2021-02-12 19:28:06 +0545

    style services caption

--- src/pages/Homepage/Services/ServiceInformationBoxCaption.js
+++ src/pages/Homepage/Services/ServiceInformationBoxCaption.js
@@ -1,8 +1,8 @@
 const ServiceInformationBoxCaption = ({ title, subtitle }) => {
   return (
-    <div>
-      <p>{title}</p>
-      <small>{subtitle}</small>
+    <div className="my-5">
+      <p className="text-yellow-500 text-2xl">{title}</p>
+      <small className="text-white text-base">{subtitle}</small>
     </div>
   );
 };
