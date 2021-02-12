def44e4ff963de80711cb983028aa415cdd3c43a
sandip satyal <sandipsatyal25@gmail.com>
2021-02-12 21:02:08 +0545

    contact section desktop view complete

--- src/pages/Homepage/Contact/ContactInformation.js
+++ src/pages/Homepage/Contact/ContactInformation.js
@@ -1,11 +1,13 @@
-const ContactInformation = ({ icon, information }) => {
+const ContactInformation = ({ icon, information, link }) => {
   return (
-    <div className="mx-5 my-5 p-5 flex bg-gray-700 hover:shadow-2xl rounded">
-      <div className="mx-2">{icon}</div>
-      <div>
-        <p>{information}</p>
+    <a href={link} target="_blank" rel="noreferrer">
+      <div className="mx-5 my-5 p-5 flex bg-gray-700 hover:shadow-2xl rounded">
+        <div className="flex mx-2">{icon}</div>
+        <div>
+          <p className="text-white">{information}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
--- src/pages/Homepage/Contact/index.js
+++ src/pages/Homepage/Contact/index.js
@@ -1,20 +1,56 @@
+import {
+  FaGoogle,
+  FaPhoneAlt,
+  FaSkype,
+  FaTwitter,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+
 import SectionHeading from "../Components/SectionHeading";
 import SectionSubHeading from "../Components/SectionSubHeading";
 import SectionTitle from "../Components/SectionTitle";
 import ContactInformation from "./ContactInformation";
 
 const ContactSection = () => {
+  const iconStyles = {
+    color: "white",
+    style: {
+      display: "flex",
+      margin: "auto",
+    },
+  };
   return (
     <section id="contact" className="bg-gray-900 text-center p-20">
       <SectionTitle title="contact me" />
       <SectionHeading heading="Ways to contact me" />
       <SectionSubHeading text="daskd ;alskd ;lakdlkasdk;ak d;ushfkjsdhfyw giuhfvj df dkl;j o" />
       <div className="flex flex-wrap my-5 mx-40 justify-around">
-        <ContactInformation icon="1" information="@sandipzzz" />
-        <ContactInformation icon="1" information="@sandipzzz" />
-        <ContactInformation icon="1" information="@sandipzzz" />
-        <ContactInformation icon="1" information="@sandipzzz" />
-        <ContactInformation icon="1" information="@sandipzzz" />
+        <ContactInformation
+          icon={<FaGoogle {...iconStyles} />}
+          information="sandipsatyal25"
+          link="mailto:sandipsatyal25@gmail.com"
+        />
+        <ContactInformation
+          icon={<FaPhoneAlt {...iconStyles} />}
+          information="9811226217"
+          link="tel:9811226217"
+        />
+        <ContactInformation
+          icon={<FaLinkedin {...iconStyles} />}
+          information="sandip-satyal"
+          link="https://www.linkedin.com/in/sandip-satyal-478854193/"
+        />
+        <ContactInformation
+          icon={<FaTwitter {...iconStyles} />}
+          information="@__Sandip__"
+          link="https://twitter.com/__Sandip__"
+        />
+        <ContactInformation
+          icon={<FaGithub {...iconStyles} />}
+          information="@Sandip-Z"
+          link="https://github.com/Sandip-Z"
+        />
       </div>
     </section>
   );
